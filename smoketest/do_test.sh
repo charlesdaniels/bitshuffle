3c4e5d626eefa844c3c5d51d422d1bfb36a16454
Charles Daniels <cdaniels@fastmail.com>
2017-12-08 18:34:15 -0500

    Smoketest now also tries very small and very large chunk sizes

--- smoketest/do_test.sh
+++ smoketest/do_test.sh
@@ -15,20 +15,72 @@ if [ ! -x "$BITSHUFFLE" ] ; then
 fi
 
 make_random () {
-	dd if=/dev/urandom of="$1" bs=1024 count=4 > /dev/null 2>&1
+	dd if=/dev/urandom of="$1" bs=1024 count=64 > /dev/null 2>&1
 }
 
 printf "Running tests...\n"
 
 TESTS_FAILED=0
-printf "Basic encode/decode test... "
 
+printf "Basic encode/decode test... "
 LOG_FILE="/tmp/$(uuidgen)"
 TEMPFILE_SRC="/tmp/$(uuidgen)"
 TEMPFILE_DST="/tmp/$(uuidgen)"
 make_random "$TEMPFILE_SRC"
 TEMPFILE_SRC_SHA="$(shasum "$TEMPFILE_SRC" 2>&1 | cut -d ' ' -f 1)"
 "$BITSHUFFLE" --encode --input "$TEMPFILE_SRC" | \
+	"$BITSHUFFLE" --decode --output "$TEMPFILE_DST" > "$LOG_FILE" 2>&1
+TEMPFILE_DST_SHA="$(shasum "$TEMPFILE_DST" 2>&1 | cut -d ' ' -f 1)"
+if [ "$TEMPFILE_DST_SHA" = "$TEMPFILE_SRC_SHA" ] ; then
+	printf "PASSED\n"
+else
+	printf "FAILED\n"
+	printf "\n"
+	printf "\t$TEMPFILE_SRC_SHA does not match $TEMPFILE_DST_SHA\n"
+	printf "\n"
+	while read -r ln  ; do
+		printf "\t$ln\n"
+	done < "$LOG_FILE"
+	printf "\n\n"
+	TESTS_FAILED="$(echo "$TESTS_FAILED + 1" | bc)"
+fi
+rm -f "$LOG_FILE"
+rm -f "$TEMPFILE_SRC"
+rm -f "$TEMPFILE_DST"
+
+printf "Basic encode/decode test (large chunk size)... "
+LOG_FILE="/tmp/$(uuidgen)"
+TEMPFILE_SRC="/tmp/$(uuidgen)"
+TEMPFILE_DST="/tmp/$(uuidgen)"
+make_random "$TEMPFILE_SRC"
+TEMPFILE_SRC_SHA="$(shasum "$TEMPFILE_SRC" 2>&1 | cut -d ' ' -f 1)"
+"$BITSHUFFLE" --encode --input "$TEMPFILE_SRC" --chunksize 16384 | \
+	"$BITSHUFFLE" --decode --output "$TEMPFILE_DST" > "$LOG_FILE" 2>&1
+TEMPFILE_DST_SHA="$(shasum "$TEMPFILE_DST" 2>&1 | cut -d ' ' -f 1)"
+if [ "$TEMPFILE_DST_SHA" = "$TEMPFILE_SRC_SHA" ] ; then
+	printf "PASSED\n"
+else
+	printf "FAILED\n"
+	printf "\n"
+	printf "\t$TEMPFILE_SRC_SHA does not match $TEMPFILE_DST_SHA\n"
+	printf "\n"
+	while read -r ln  ; do
+		printf "\t$ln\n"
+	done < "$LOG_FILE"
+	printf "\n\n"
+	TESTS_FAILED="$(echo "$TESTS_FAILED + 1" | bc)"
+fi
+rm -f "$LOG_FILE"
+rm -f "$TEMPFILE_SRC"
+rm -f "$TEMPFILE_DST"
+
+printf "Basic encode/decode test (small chunk size)... "
+LOG_FILE="/tmp/$(uuidgen)"
+TEMPFILE_SRC="/tmp/$(uuidgen)"
+TEMPFILE_DST="/tmp/$(uuidgen)"
+make_random "$TEMPFILE_SRC"
+TEMPFILE_SRC_SHA="$(shasum "$TEMPFILE_SRC" 2>&1 | cut -d ' ' -f 1)"
+"$BITSHUFFLE" --encode --input "$TEMPFILE_SRC" --chunksize 8 | \
 	"$BITSHUFFLE" --decode --output "$TEMPFILE_DST" > "$LOG_FILE" 2>&1
 TEMPFILE_DST_SHA="$(shasum "$TEMPFILE_DST" 2>&1 | cut -d ' ' -f 1)"
 if [ "$TEMPFILE_DST_SHA" = "$TEMPFILE_SRC_SHA" ] ; then
