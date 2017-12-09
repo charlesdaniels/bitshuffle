6a858c4a9dd8a853c673b671ff29bcc74406099f
Joshua Nelson <jyn514@gmail.com>
2017-12-09 17:07:50 -0500

    Refactored do_test to be much easier to add to
    Added function do_test which abstracts most of the
    necessary boilerplate

--- smoketest/do_test.sh
+++ smoketest/do_test.sh
@@ -18,87 +18,50 @@ make_random () {
 	dd if=/dev/urandom of="$1" bs=1024 count=64 > /dev/null 2>&1
 }
 
+do_test () {
+	# requires $1 to be a string literal of the test you wish to execute.
+	# see below for examples
+	LOG_FILE="/tmp/$(uuidgen)"
+	TEMPFILE_SRC="/tmp/$(uuidgen)"
+	TEMPFILE_DST="/tmp/$(uuidgen)"
+	make_random "$TEMPFILE_SRC"
+	TEMPFILE_SRC_SHA="$(shasum "$TEMPFILE_SRC" 2>&1 | cut -d ' ' -f 1)"
+	eval "$1"
+	TEMPFILE_DST_SHA="$(shasum "$TEMPFILE_DST" 2>&1 | cut -d ' ' -f 1)"
+	if [ "$TEMPFILE_DST_SHA" = "$TEMPFILE_SRC_SHA" ] ; then
+		echo "PASSED"
+	else
+		echo "FAILED"
+		echo
+		printf "\t$TEMPFILE_SRC_SHA does not match $TEMPFILE_DST_SHA\n"
+		echo
+		while read -r ln  ; do
+			printf "\t$ln\n"
+		done < "$LOG_FILE"
+		printf "\n\n"
+		TESTS_FAILED="$(echo "$TESTS_FAILED + 1" | bc)"
+	fi
+	rm -f "$LOG_FILE"
+	rm -f "$TEMPFILE_SRC"
+	rm -f "$TEMPFILE_DST"
+}
+
 printf "Running tests...\n"
 
 TESTS_FAILED=0
 
 printf "Basic encode/decode test... "
-LOG_FILE="/tmp/$(uuidgen)"
-TEMPFILE_SRC="/tmp/$(uuidgen)"
-TEMPFILE_DST="/tmp/$(uuidgen)"
-make_random "$TEMPFILE_SRC"
-TEMPFILE_SRC_SHA="$(shasum "$TEMPFILE_SRC" 2>&1 | cut -d ' ' -f 1)"
-"$BITSHUFFLE" --encode --input "$TEMPFILE_SRC" | \
-	"$BITSHUFFLE" --decode --output "$TEMPFILE_DST" > "$LOG_FILE" 2>&1
-TEMPFILE_DST_SHA="$(shasum "$TEMPFILE_DST" 2>&1 | cut -d ' ' -f 1)"
-if [ "$TEMPFILE_DST_SHA" = "$TEMPFILE_SRC_SHA" ] ; then
-	printf "PASSED\n"
-else
-	printf "FAILED\n"
-	printf "\n"
-	printf "\t$TEMPFILE_SRC_SHA does not match $TEMPFILE_DST_SHA\n"
-	printf "\n"
-	while read -r ln  ; do
-		printf "\t$ln\n"
-	done < "$LOG_FILE"
-	printf "\n\n"
-	TESTS_FAILED="$(echo "$TESTS_FAILED + 1" | bc)"
-fi
-rm -f "$LOG_FILE"
-rm -f "$TEMPFILE_SRC"
-rm -f "$TEMPFILE_DST"
+do_test '"$BITSHUFFLE" --encode --input "$TEMPFILE_SRC" | \
+	"$BITSHUFFLE" --decode --output "$TEMPFILE_DST" > "$LOG_FILE" 2>&1'
 
 printf "Basic encode/decode test (large chunk size)... "
-LOG_FILE="/tmp/$(uuidgen)"
-TEMPFILE_SRC="/tmp/$(uuidgen)"
-TEMPFILE_DST="/tmp/$(uuidgen)"
-make_random "$TEMPFILE_SRC"
-TEMPFILE_SRC_SHA="$(shasum "$TEMPFILE_SRC" 2>&1 | cut -d ' ' -f 1)"
-"$BITSHUFFLE" --encode --input "$TEMPFILE_SRC" --chunksize 16384 | \
-	"$BITSHUFFLE" --decode --output "$TEMPFILE_DST" > "$LOG_FILE" 2>&1
-TEMPFILE_DST_SHA="$(shasum "$TEMPFILE_DST" 2>&1 | cut -d ' ' -f 1)"
-if [ "$TEMPFILE_DST_SHA" = "$TEMPFILE_SRC_SHA" ] ; then
-	printf "PASSED\n"
-else
-	printf "FAILED\n"
-	printf "\n"
-	printf "\t$TEMPFILE_SRC_SHA does not match $TEMPFILE_DST_SHA\n"
-	printf "\n"
-	while read -r ln  ; do
-		printf "\t$ln\n"
-	done < "$LOG_FILE"
-	printf "\n\n"
-	TESTS_FAILED="$(echo "$TESTS_FAILED + 1" | bc)"
-fi
-rm -f "$LOG_FILE"
-rm -f "$TEMPFILE_SRC"
-rm -f "$TEMPFILE_DST"
+do_test '"$BITSHUFFLE" --encode --input "$TEMPFILE_SRC" --chunksize 16384 | \
+	"$BITSHUFFLE" --decode --output "$TEMPFILE_DST" > "$LOG_FILE" 2>&1'
+
 
 printf "Basic encode/decode test (small chunk size)... "
-LOG_FILE="/tmp/$(uuidgen)"
-TEMPFILE_SRC="/tmp/$(uuidgen)"
-TEMPFILE_DST="/tmp/$(uuidgen)"
-make_random "$TEMPFILE_SRC"
-TEMPFILE_SRC_SHA="$(shasum "$TEMPFILE_SRC" 2>&1 | cut -d ' ' -f 1)"
-"$BITSHUFFLE" --encode --input "$TEMPFILE_SRC" --chunksize 8 | \
-	"$BITSHUFFLE" --decode --output "$TEMPFILE_DST" > "$LOG_FILE" 2>&1
-TEMPFILE_DST_SHA="$(shasum "$TEMPFILE_DST" 2>&1 | cut -d ' ' -f 1)"
-if [ "$TEMPFILE_DST_SHA" = "$TEMPFILE_SRC_SHA" ] ; then
-	printf "PASSED\n"
-else
-	printf "FAILED\n"
-	printf "\n"
-	printf "\t$TEMPFILE_SRC_SHA does not match $TEMPFILE_DST_SHA\n"
-	printf "\n"
-	while read -r ln  ; do
-		printf "\t$ln\n"
-	done < "$LOG_FILE"
-	printf "\n\n"
-	TESTS_FAILED="$(echo "$TESTS_FAILED + 1" | bc)"
-fi
-rm -f "$LOG_FILE"
-rm -f "$TEMPFILE_SRC"
-rm -f "$TEMPFILE_DST"
+do_test '"$BITSHUFFLE" --encode --input "$TEMPFILE_SRC" --chunksize 8 | \
+	"$BITSHUFFLE" --decode --output "$TEMPFILE_DST" > "$LOG_FILE" 2>&1'
 
 printf "\n"
 printf "$TESTS_FAILED tests failed\n"
