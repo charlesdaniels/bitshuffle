682e30984abc54254431cd2d0b4e05985ae8f177
Aidan Fray <afray@hotmail.co.uk>
2017-12-09 12:22:43 +0000

    Changes; -Added --version flag

--- bitshuffle.py
+++ bitshuffle.py
@@ -27,6 +27,10 @@ stdout = sys.stdout
 stdin = sys.stdin
 
 
+# Change this version variable to change the --version output
+program_version = 1.0
+
+
 def encode_data(data, chunksize, compresslevel):
     """encode_data
 
@@ -126,6 +130,10 @@ def main():
     iochoice.add_argument("--decode", "-d", "-D", action="store_true",
                           help="Extract a BitShuffle data packet.")
 
+    # Added to the iochoice because the '-v' parameter should be able to be used on its own
+    iochoice.add_argument("--version", "-v", action="store_true",
+                        help="Displays the current version of bitshuffle")
+
     parser.add_argument("--chunksize", "-c", type=int, default=2048,
                         help="Chunk size in bytes")
 
@@ -136,6 +144,10 @@ def main():
                         help="Editor to use for pasting packets")
 
     args = parser.parse_args()
+
+    if args.version:
+        print("Version: bitshuffle v{0}".format(program_version))
+        exit(0)
 
     if args.filename is None:
         args.filename = os.path.basename(args.input)
