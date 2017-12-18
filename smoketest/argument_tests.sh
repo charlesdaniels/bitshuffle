2e855aa15c75b3b25633294e37f7604eb39da9de
Joshua Nelson <jyn514@gmail.com>
2017-12-18 11:52:15 -0500

    Add argument tests

---
+++ smoketest/argument_tests.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+# .SHELLDOC
+
+# Smoketests for argument handling. Exit code is # of failed tests.
+# Heavily influenced by existing smoketests.
+
+# .ENDOC
+
+TEST_DIR="`readlink -e $(dirname "$0")`"
+BITSHUFFLE="$TEST_DIR/../bitshuffle/bitshuffle.py"
+TESTS_FAILED=0
+
+if [ ! -x "$BITSHUFFLE" ] ; then
+        echo "FATAL: '$BITSHUFFLE' does not exist"
+        exit 9999
+fi
+
+# Note: of necessity, the following can only test non-interactive
+# situations. Interactive tests are not a priority at the current moment.
+
+print_log_file() {
+# takes 1 parameter, a text file
+        while read -r line; do
+                printf "\t$line\n"
+        done < $1
+        printf "\n\n"
+}
+
+
+expect_usage_error () {
+        LOG_FILE="/tmp/`uuidgen`"
+        $BITSHUFFLE $@ > "$LOG_FILE" 2>&1
+        if grep -q "usage: .*" $LOG_FILE; then
+                echo "PASSED"
+        else
+                printf "FAILED\n\n"
+
+                printf "\t'usage: .*' does not match logfile\n\n" 
+                print_log_file $LOG_FILE
+                TESTS_FAILED=`echo "$TESTS_FAILED + 1" | bc`
+                rm -f $LOG_FILE
+        fi
+}
+
+printf "Running tests...\n\n"
+
+printf "When run with no args, prints help... "
+expect_usage_error
+
+printf "When run with '-h', prints help... "
+expect_usage_error -h
+
+printf "When given bad compresstype, prints help... "
+expect_usage_error -t gmander
+
+printf "When given bad editor, prints editor not found... "
+LOG_FILE="/tmp/`uuidgen`"
+$BITSHUFFLE --editor /nonexistent > "$LOG_FILE" 2>&1
+if [ "`cat $LOG_FILE`" = "Editor /nonexistent not found" ]; then
+        echo "PASSED"
+else
+        printf "FAILED\n\n"
+
+        printf "'Editor /nonexistent not found' does not match logfile\n\n"
+
+        print_log_file $LOG_FILE
+        TESTS_FAILED="`echo $TESTS_FAILED + 1 | bc`"
+        rm -f $LOG_FILE
+fi
+
+LOG_FILE="/tmp/`uuidgen`"
+printf "When given bad input, prints file not found... "
+$BITSHUFFLE --input /nonexistent/nope > "$LOG_FILE" 2>&1
+if [ "`cat $LOG_FILE`" = "Error: Input file not found" ]; then
+    echo "PASSED"
+else
+    printf "FAILED\n\n"
+
+    printf "\t'Error: Input file not found' \
+             does not match logfile\n\n"
+
+    print_log_file $LOG_FILE
+    TESTS_FAILED=`echo $TESTS_FAILED + 1 | bc`
+    rm -f $LOG_FILE
+fi
+
+printf "\n$TESTS_FAILED tests failed.\n"
+exit $TESTS_FAILED
+
---
+++ smoketest/library_tests.sh
@@ -0,0 +1,95 @@
+#!/bin/sh
+
+# .SHELLDOC
+
+# Basic smoketests for BitShuffle. Exit code is # of failed tests.
+
+# .ENDOC
+
+PARENT_DIR="$(dirname "$0")"
+BITSHUFFLE="$PARENT_DIR/../bitshuffle/bitshuffle.py"
+BITSHUFFLE2="python2 $BITSHUFFLE"
+BITSHUFFLE3="python3 $BITSHUFFLE"
+if [ ! -x "$BITSHUFFLE" ] ; then
+	echo "FATAL: '$BITSHUFFLE' does not exist"
+	exit 9999
+fi
+
+make_random () {
+	dd if=/dev/urandom of="$1" bs=1024 count=64 > /dev/null 2>&1
+}
+
+do_test () {
+	# requires $1 to be a string literal of the test you wish to execute.
+	# see below for examples
+	LOG_FILE="/tmp/$(uuidgen)"
+	TEMPFILE_SRC="/tmp/$(uuidgen)"
+	TEMPFILE_DST="/tmp/$(uuidgen)"
+	make_random "$TEMPFILE_SRC"
+	TEMPFILE_SRC_SHA="$(shasum "$TEMPFILE_SRC" 2>&1 | cut -d ' ' -f 1)"
+	eval "$1" > "$LOG_FILE" 2>&1 
+	TEMPFILE_DST_SHA="$(shasum "$TEMPFILE_DST" 2>&1 | cut -d ' ' -f 1)"
+	if [ "$TEMPFILE_DST_SHA" = "$TEMPFILE_SRC_SHA" ] ; then
+		echo "PASSED"
+	else
+		printf "FAILED\n\n"
+
+		printf "\t$TEMPFILE_SRC_SHA does not match $TEMPFILE_DST_SHA\n"
+		echo
+		while read -r ln  ; do
+			printf "\t$ln\n"
+		done < "$LOG_FILE"
+		printf "\n\n"
+		TESTS_FAILED="$(echo "$TESTS_FAILED + 1" | bc)"
+	fi
+	rm -f "$LOG_FILE"
+	rm -f "$TEMPFILE_SRC"
+	rm -f "$TEMPFILE_DST"
+}
+
+all_tests () {
+
+    printf "Basic encode/decode test... "
+    do_test '$BITSHUFFLE --encode --input "$TEMPFILE_SRC" | \
+        $BITSHUFFLE --decode --output "$TEMPFILE_DST" > "$LOG_FILE" 2>&1'
+
+    printf "Basic encode/decode test (large chunk size)... "
+    do_test '$BITSHUFFLE --encode --input "$TEMPFILE_SRC" --chunksize 16384 | \
+        $BITSHUFFLE --decode --output "$TEMPFILE_DST" > "$LOG_FILE" 2>&1'
+
+
+    printf "Basic encode/decode test (small chunk size)... "
+    do_test '$BITSHUFFLE --encode --input "$TEMPFILE_SRC" --chunksize 8 | \
+        $BITSHUFFLE --decode --output "$TEMPFILE_DST" > "$LOG_FILE" 2>&1'
+
+
+    printf "Double encode/decode test... "
+    do_test '$BITSHUFFLE --encode --input "$TEMPFILE_SRC" | \
+        $BITSHUFFLE --encode | $BITSHUFFLE --decode | \
+        $BITSHUFFLE --decode --output "$TEMPFILE_DST" > "$LOG_FILE" 2>&1'
+
+    printf "GZIP test... "
+    do_test '$BITSHUFFLE --encode --compresstype "gzip" --input "$TEMPFILE_SRC" | \
+        $BITSHUFFLE --decode --output "$TEMPFILE_DST" > "$LOG_FILE" 2>&1'
+
+
+    printf "BZIP test... "
+    do_test '$BITSHUFFLE --encode --compresstype "bz2" --input "$TEMPFILE_SRC" | \
+        $BITSHUFFLE --decode --output "$TEMPFILE_DST" > "$LOG_FILE" 2>&1'
+        
+}
+
+echo "Running tests..."
+
+TESTS_FAILED=0
+
+printf "\nRunning Python2 tests...\n"
+BITSHUFFLE="$BITSHUFFLE2"
+all_tests
+
+printf "\nRunning Python3 tests...\n"
+BITSHUFFLE="$BITSHUFFLE3"
+all_tests
+
+printf "\n$TESTS_FAILED tests failed\n"
+exit $TESTS_FAILED
